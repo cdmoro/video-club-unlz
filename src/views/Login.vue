d4bd4a68494f0ca0609bcd86b97b83d555eee355
Carlos <carlosbonadeo@gmail.com>
2021-05-20 15:40:40 -0300

    Login style

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,94 +1,99 @@
 <template>
   <div class="login-page">
     <v-container>
-      <h2>Ingreso</h2>
-      <v-alert
-        v-if="errorMessage.length > 0"
-        icon="mdi-account"
-        type="error"
-        class="mt-4"
-        >{{ errorMessage }}</v-alert
-      >
+      <v-row justify="center">
+        <v-col sm="8" md="4">
+          <v-card class="my-4">
+            <v-card-title>Ingreso</v-card-title>
+            <v-card-text>
+              <v-alert v-if="errorMessage.length > 0" type="error">{{
+                errorMessage
+              }}</v-alert>
+              <v-text-field
+                label="Usuario"
+                autocomplete="off"
+                hide-details="auto"
+                :error="errorMessage.length > 0"
+                v-model="user"
+              ></v-text-field>
 
-      <v-card class="my-4">
-        <!-- <v-card-title>Ingreso</v-card-title> -->
-        <v-card-text>
-        <v-text-field
-          label="Usuario"
-          autocomplete="off"
-          hide-details="auto"
-          :error="errorMessage.length > 0"
-          v-model="user"
-        ></v-text-field>
+              <v-text-field
+                class="mt-2"
+                label="Contraseña"
+                :type="showPassword ? 'text' : 'password'"
+                hide-details="auto"
+                :error="errorMessage.length > 0"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                v-model="pass"
+                @click:append="showPassword = !showPassword"
+              ></v-text-field>
+            </v-card-text>
+          </v-card>
 
-        <v-text-field
-          label="Contraseña"
-          type="password"
-          hide-details="auto"
-          :error="errorMessage.length > 0"
-          v-model="pass"
-        ></v-text-field>
-        </v-card-text>
-      </v-card>
+          <v-btn class="mb-2" color="primary" dark @click="login"
+            >Ingresar</v-btn
+          >
+          <div>
+            ¿Olvidaste tu contraseña?
 
-      <v-btn class="mb-2" color="primary" dark @click="login"
-        >Ingresar</v-btn
-      >
-      <div>
-        ¿Olvidaste tu contraseña?
+            <v-dialog max-width="600">
+              <template v-slot:activator="{ on, attrs }">
+                <a
+                  href="#"
+                  class="white--text"
+                  v-bind="attrs"
+                  v-on="on"
+                  @click="recuperarMail = ''"
+                  >Recuperar contraseña</a
+                >
+              </template>
+              <template v-slot:default="dialog">
+                <v-card>
+                  <v-card-title> Recuperar contraseña </v-card-title>
 
-        <v-dialog max-width="600">
-          <template v-slot:activator="{ on, attrs }">
-            <a
-              href="#"
-              class="white--text"
-              v-bind="attrs"
-              v-on="on"
-              @click="recuperarMail = ''"
-              >Recuperar contraseña</a
+                  <v-card-text class="mb-2">
+                    <v-text-field
+                      label="Ingresá tu dirección de mail"
+                      autocomplete="off"
+                      hide-details="auto"
+                      type="mail"
+                      v-model="recuperarMail"
+                      autofocus
+                    ></v-text-field>
+                  </v-card-text>
+
+                  <v-card-actions
+                    class="justify-end flex-column-reverse flex-sm-row"
+                  >
+                    <v-btn
+                      text
+                      class="mt-3 mt-sm-0"
+                      @click="dialog.value = false"
+                      >Cancelar</v-btn
+                    >
+
+                    <v-btn
+                      color="primary"
+                      @click="
+                        dialog.value = false;
+                        snackbar = true;
+                      "
+                      >Recuperar contraseña</v-btn
+                    >
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
+          </div>
+
+          <div>
+            ¿No tenés cuenta?
+            <router-link to="/registro" class="white--text"
+              >Registrate</router-link
             >
-          </template>
-          <template v-slot:default="dialog">
-            <v-card>
-              <v-card-title>
-                Recuperar contraseña
-              </v-card-title>
-
-              <v-card-text class="mb-2">
-                <v-text-field
-                  label="Ingresá tu dirección de mail"
-                  autocomplete="off"
-                  hide-details="auto"
-                  type="mail"
-                  v-model="recuperarMail"
-                  autofocus
-                ></v-text-field>
-              </v-card-text>
-
-              <v-card-actions
-                class="justify-end flex-column-reverse flex-sm-row"
-              >
-                <v-btn text class="mt-3 mt-sm-0" @click="dialog.value = false"
-                  >Cancelar</v-btn
-                >
-
-                <v-btn
-                  color="primary"
-                  @click="
-                    dialog.value = false;
-                    snackbar = true;
-                  "
-                  >Recuperar contraseña</v-btn
-                >
-              </v-card-actions>
-            </v-card>
-          </template>
-        </v-dialog>
-      </div>
-
-      <div>
-        ¿No tenés cuenta? <router-link to="/registro" class="white--text">Registrate</router-link>
-      </div>
+          </div>
+        </v-col></v-row
+      >
 
       <v-snackbar v-model="snackbar" :timeout="4000" right bottom>
         ¡Se envió un mail a tu casilla de correo!
@@ -106,6 +111,7 @@ export default {
       errorMessage: "",
       user: "",
       pass: "",
+      showPassword: false,
       recuperarMail: "",
       snackbar: false,
     };
